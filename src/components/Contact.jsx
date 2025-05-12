54d75dd720f036756782c639c297166a45988397
Jenni <jennimmartin@outlook.com>
2025-05-12 17:23:51 +0200

    Formatting of Contact and Gallery Sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,12 @@ const Contact = () => {
   return (
     <div className="bg-sky-50 py-30">
       <div className="mx-auto max-w-7xl" id="contact">
-        <h3>contact</h3>
-        <p>Une question à nous poser ?</p>
+        <h3 className="text-center text-2xl text-bold tracking-wider text-sky-950 pb-8 md:text-3xl">
+          Contact
+        </h3>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
+          Une question à nous poser ?
+        </p>
       </div>
     </div>
   );
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,9 +2,15 @@ const Gallery = () => {
   return (
     <div>
       <div className="mx-auto max-w-7xl py-30" id="galerie">
-        <h2>Galerie</h2>
-        <p>Nous pensons que nos projets passés parlent d'eux-mêmes. </p>
-        <p>Votre projet sera-t-il le prochain à être présenté ?</p>
+        <h2 className="text-center text-2xl text-bold tracking-wider text-sky-950 pb-8 md:text-3xl">
+          Galerie
+        </h2>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
+          Nous pensons que nos projets passés parlent d'eux-mêmes.
+        </p>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
+          Votre projet sera-t-il le prochain à être présenté ?
+        </p>
       </div>
     </div>
   );
