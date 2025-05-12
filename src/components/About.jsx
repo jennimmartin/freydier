e1f35fdf38be897f69063fd55e676d9be4f0961b
Jenni <jennimmartin@outlook.com>
2025-05-12 17:17:23 +0200

    Formatting About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,27 @@
 const About = () => {
   return (
-    <div className="bg-sky-50 py-30">
+    <div className="bg-sky-50 py-20">
       <div className="mx-auto max-w-7xl">
-        <h2>Qui sommes-nous ?</h2>
-        <p>
+        <h2 className="text-center text-2xl text-bold tracking-wider text-sky-950 pb-8 md:text-3xl">
+          Qui sommes-nous ?
+        </h2>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
           Freydier Carrelage est basé à Hautecour depuis 2001. Nous sommes
           passionnés par la qualité du carrelage et du dallage pour les
           particuliers et les entreprises de toute la Tarentaise et au-delà.
         </p>
-        <p>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
           Nous avons l'habitude de travailler pour les collectivités locales,
           les professionnels, les architectes, les bureaux d'études, les
           particuliers et les copropriétés, en posant des carrelages, des
           faïences et des mosaïques et en traitant l'étanchéité des sols
           (hydrofuge et oléofuge).
         </p>
-        <p>
+        <p className="text-center pb-4 tracking-wide  px-8 md:text-lg">
           Si les carreaux et les dallages font sensation, nous sommes conscients
           de l'importance d'un bon travail préparatoire.
         </p>
-        <p>
+        <p className="text-center pb-4 tracking-wide px-8 md:text-lg">
           La perfection est au cœur de notre travail et nous avons développé un
           sens aigu du détail au cours des 24 dernières années.
         </p>
