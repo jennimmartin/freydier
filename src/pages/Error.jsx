f4d18a8a94f66295ca4be4398776cacf788a1bc4
Jenni <jennimmartin@outlook.com>
2025-05-20 17:27:58 +0200

    error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import { GoHome } from "react-icons/go";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Whoops something went wrong</h1>
-      <Link to="/">Home</Link>
+    <div className="min-h-dvh grid text-center justify-center bg-gray-100 py-20">
+      <div className="items-center">
+        <Link to="/">
+          <GoHome className="h-20 w-20 place-self-center hover:text-gray-500 duration-300 " />
+        </Link>
+
+        <h1 className="text-2xl tracking-wide pt-8 text-gray-900">
+          Oh non, il y a un problème, cliquez pour revenir en arrière !
+        </h1>
+      </div>
     </div>
   );
 };
