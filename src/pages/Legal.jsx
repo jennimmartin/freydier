c06674406cdaa6e5f24a241e1e999d8d90cec5b0
Jenni <jennimmartin@outlook.com>
2025-05-20 17:32:12 +0200

    Basic legal page

--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import { GoHome } from "react-icons/go";
 
 const Legal = () => {
   return (
-    <div>
-      <h1>Mentions legal</h1>
-      <Link to="/">Home</Link>
+    <div className="min-h-dvh grid text-center justify-center bg-gray-100 py-20">
+      <div className="items-center">
+        <Link to="/">
+          <GoHome className="h-20 w-20 place-self-center hover:text-gray-500 duration-300 " />
+        </Link>
+        <h3 className="text-center text-2xl text-bold tracking-wider text-sky-700 pb-8 md:text-3xl">
+          Mentions legal
+        </h3>
+      </div>
     </div>
   );
 };
